a4a6d3696a0f2ef150349edf0be17c1de8e6322d
Shashivardhan3 <107747614+Shashivardhan3@users.noreply.github.com>
2023-07-31 11:40:31 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -10,3 +10,10 @@ def data_render(request):
 def conditions(request):
     d={'a':24,'b':56,'c':148}
     return render (request,'conditions.html',context=d)
+
+
+def for_loop(request):
+
+    d={'name':'shashi','age':24,'hobbies':['cricket','football','hockey']}
+    return render (request,'for_loop.html',context=d)
+
